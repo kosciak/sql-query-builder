5a03a0864463593c4280e66364260e2d89dcbef8
Wojciech Pietrzok <kosciak@kosciak.net>
2022-09-25 18:35:23 +0200

    sql - imports for And, Or

--- sql/__init__.py
+++ sql/__init__.py
@@ -4,12 +4,13 @@ from .enums import Order, Nulls, Join
 
 from .core import Alias
 from .core import Aggregate
+from .core import And, Or
 
 from .tables import Columns, Column, Table
 
 from .parameters import QmarkParameter, FormatParameter, NamedParameter, PyformatParameter, NumericParameter
 from .parameters import get_parameters_builder
 
-from .queries import CreateTable, CreateIndex
+from .queries import CreateTable, CreateIndex, DropTable, DropIndex
 from .queries import Select, Insert, Update, Delete
 
